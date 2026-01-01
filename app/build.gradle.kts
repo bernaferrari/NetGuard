f66a42470740ef99bb241ada71f01eaf41920c2e
Bernardo Ferrari <bernaferrari2@gmail.com>
2025-12-31 22:09:12 -0300

    Remove deprecated build flags and unused deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -147,7 +147,6 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.swiperefreshlayout)
-    implementation(libs.androidx.localbroadcastmanager)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.navigation3.ui)
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
